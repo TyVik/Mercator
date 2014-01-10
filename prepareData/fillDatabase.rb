4be9f59683be811ec7340afe69dfe3a1fead409b
Viktor Tyshchenko <tyvik8@gmail.com>
2014-01-10 11:00:51 +0400

    Fix kml import. Add kml for Russia, USA.

--- prepareData/fillDatabase.rb
+++ prepareData/fillDatabase.rb
@@ -1,89 +1,148 @@
 #!/usr/bin/env ruby
+# coding: utf-8
 
 require 'rexml/document'
 require './gmap.rb'
 require 'pg'
 include REXML
 
+$connection = PG::Connection.new(:host => '95.85.47.11', :port => 5432, :dbname => 'mercator', :user => 'mercator', :password => 'mercator')
 $encoder = GMapPolylineEncoder.new()
-xmlfile = File.new("countries_world.xml")
-xmldoc = Document.new(xmlfile)
 
-$connection = PG::Connection.new( :host => '127.0.0.1', :port => 5432,
-    :dbname => 'mercator', :user => 'mercator', :password => 'mercator')
-$connection.prepare('stm', 'INSERT INTO "Countries"("Name", "Polygon", "Answer") values ($1, ARRAY[$2], $3)')
+class KmlWriter
+  @fileName     # name of file for import
+  @tableName    # name of table for kml
+  @name         # name of map for menu
+  @center       # center map
+  @position     # default regions position
+  @zoom         # zoom map
+  @precision    # accuracy answer
 
-def processPolygon(str)
-    coordinates = str.split
-    data = []
-    coordinates.each {
-      |coord|
-        arr = coord.split ","
-        data.push([arr[1].to_f, arr[0].to_f])
-    }
-    return $encoder.encode(data)
+  def initialize(file)
+    @fileName = file
+    parts = file.match('^kml\/(?<name>\S+)\.kml$')
+    @tableName = "map_#{parts['name']}".downcase # postgres dosn`t like upper case
+
+    puts "Process file %s" % [@fileName]
+    puts "Enter name of maps"
+    @name = STDIN.gets.chomp
+    puts "Enter center (format %f, %f)"
+    @center = STDIN.gets.chomp
+    puts "Enter default region position (format %f, %f)"
+    @position = STDIN.gets.chomp
+    puts "Enter zoom"
+    @zoom = STDIN.gets.chomp
+    puts "Enter precision"
+    @precision = STDIN.gets.chomp.to_f
+  end
+
+  def run()
+    createTable()
+    fillTable()
+    registerTable()
+  rescue RuntimeError => error
+    puts "Exception rised while process file %s" % [file]
+    puts error.message
+    puts error.backtrace
+  end
+
+  def createTable()
+    $connection.exec('CREATE SEQUENCE "%s_seq"
+      INCREMENT 1
+      MINVALUE 1
+      MAXVALUE 9223372036854775807
+      START 1
+      CACHE 1;' % [@tableName])
+    $connection.exec('CREATE TABLE "%s"(
+      "id" integer NOT NULL DEFAULT nextval(\'%s_seq\'),
+      "name" character varying(50) NOT NULL,
+      "polygon" character varying[],
+      "available" boolean,
+      "answer" character varying(50),
+      CONSTRAINT "%s_id" PRIMARY KEY ("id" )
+      ) WITH (
+        OIDS=FALSE
+      );' % [@tableName, @tableName, @tableName])
+  end
+
+  def fillTable()
+      def getPolygons(polygons)
+          def processPolygon(str)
+              coordinates = str.split
+              data = []
+              coordinates.each do |coord|
+                  arr = coord.split ","
+                  data.push([arr[1].to_f, arr[0].to_f])
+              end
+              return $encoder.encode(data)
+          end
+
+          result = []
+          polygons.elements.each do |polygon|
+              result = result + [processPolygon(polygon.text)[:points]]
+          end
+          return result
+      end
+
+      # get south-west and north-east rectangle for answer
+      def getAnswer(polygons, precision)
+          s = nil
+          w = nil
+          n = nil
+          e = nil
+          init = false
+          polygons.elements.each do |polygon|
+              coordinates = polygon.text.split
+              coordinates.each do |coord|
+                  arr = coord.split ","
+                  if (!init)
+                      s = arr[1].to_f
+                      w = arr[0].to_f
+                      n = s
+                      e = w
+                      init = true
+                  else
+                      s = s > arr[1].to_f ? arr[1].to_f : s
+                      n = n < arr[1].to_f ? arr[1].to_f : n
+                      w = w > arr[0].to_f ? arr[0].to_f : w
+                      e = e > arr[0].to_f ? arr[0].to_f : e
+                   end
+              end
+          end
+          # give a human a chance to get into the square
+          s = (s-precision).round(4)
+          w = (w-precision).round(4)
+          n = (n+precision).round(4)
+          e = (e+precision).round(4)
+          return "{#{s}, #{w},#{n},#{e}}"
+      end
+
+    xmlFile = File.new(@fileName)
+    insertStm = 'insertStm_%s' % [@tableName]
+    $connection.prepare(insertStm, 'INSERT INTO %s (name, polygon, answer) values ($1, ARRAY[$2], $3)' % [@tableName])
+    xmlDoc = Document.new(xmlFile)
+
+    # for each region
+    xmlDoc.elements.each("kml/Document/Placemark") do |region|
+        name = region.elements["name"].text
+        puts "Processing " + name
+        polygons = region.elements["MultiGeometry"] ? region.elements["MultiGeometry"] : region
+        coordinates = polygons.elements['Polygon/outerBoundaryIs/LinearRing']
+        gmapPolygon = getPolygons(coordinates)
+        answer = getAnswer(coordinates, @precision)
+        $connection.exec_prepared(insertStm, [name, gmapPolygon, answer])
+    end
+  end
+
+  def registerTable()
+    $connection.exec("INSERT INTO maps (table_name, name, center, zoom, default_position) values ('%s', '%s', point(%s), %d, point(%s))" % 
+      [@tableName, @name, @center, @zoom, @position])
+  end
 end
 
-def getPolygons(node)
-    polygons = node.elements["MultiGeometry"]
-    result = []
-    polygons.each_element('Polygon/outerBoundaryIs/LinearRing/coordinates') {
-      |polygon|
-        result = result + [processPolygon(polygon.text)[:points]]
-    }
-    return result
-end
 
-# get south-west and north-east rectangle for answer
-def getAnswer(node)
-    s = nil
-    w = nil
-    n = nil
-    e = nil
-    init = false
-    polygons = node.elements["MultiGeometry"]
-    polygons.each_element('Polygon/outerBoundaryIs/LinearRing/coordinates') {
-      |polygon|
-        coordinates = polygon.text.split
-        coordinates.each {
-          |coord|
-            arr = coord.split ","
-            if (!init)
-               s = arr[1].to_f
-               w = arr[0].to_f
-               n = s
-               e = w
-               init = true
-             else
-               if s > arr[1].to_f
-                 s = arr[1].to_f
-               end
-               if n < arr[1].to_f
-                 n = arr[1].to_f
-               end
-               if w > arr[0].to_f
-                 w = arr[0].to_f
-               end
-               if e < arr[0].to_f
-                 e = arr[0].to_f
-               end
-             end
-        }
-    }
-    # give a human a chance to get into the square
-    s = (s-1).round(4)
-    w = (w-1).round(4)
-    n = (n+1).round(4)
-    e = (e+1).round(4)
-    return "{#{s}, #{w},#{n},#{e}}"
+files = ARGV.empty? ? Dir.glob('kml/*.kml') : ARGV
+files.each do |file|
+  kml = KmlWriter.new(file)
+  kml.run()
 end
-
-# for each country
-xmldoc.elements.each("kml/Document/Placemark"){
-  |e|
-    name = e.elements["name"].text
-    puts "Processing " + name
-    gmapPolygon = getPolygons(e)
-    answer = getAnswer(e)
-    $connection.exec_prepared('stm', [name, gmapPolygon, answer])
-}
---
+++ prepareData/mercator.sql
@@ -0,0 +1,153 @@
+--
+-- PostgreSQL database cluster dump
+--
+
+-- Started on 2014-01-09 17:11:02 MSK
+
+\connect postgres
+
+SET default_transaction_read_only = off;
+
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+
+--
+-- Roles
+--
+
+CREATE ROLE mercator;
+ALTER ROLE mercator WITH NOSUPERUSER INHERIT NOCREATEROLE NOCREATEDB LOGIN NOREPLICATION PASSWORD 'md5028783a1817e48ba1bb7a8c47f24aaf3' VALID UNTIL 'infinity';
+
+
+
+
+
+
+--
+-- Database creation
+--
+
+CREATE DATABASE mercator WITH TEMPLATE = template0 OWNER = mercator;
+
+\connect mercator
+
+SET default_transaction_read_only = off;
+
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 9.1.9
+-- Dumped by pg_dump version 9.1.11
+-- Started on 2014-01-09 17:11:05 MSK
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- TOC entry 163 (class 3079 OID 11677)
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: 
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- TOC entry 1899 (class 0 OID 0)
+-- Dependencies: 163
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 161 (class 1259 OID 16387)
+-- Dependencies: 5
+-- Name: maps_seq; Type: SEQUENCE; Schema: public; Owner: mercator
+--
+
+CREATE SEQUENCE maps_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.maps_seq OWNER TO mercator;
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- TOC entry 162 (class 1259 OID 16392)
+-- Dependencies: 1783 1784 1785 1786 5
+-- Name: maps; Type: TABLE; Schema: public; Owner: mercator; Tablespace: 
+--
+
+CREATE TABLE maps (
+    id integer DEFAULT nextval('maps_seq'::regclass) NOT NULL,
+    table_name character varying(15) NOT NULL,
+    name character varying(15) NOT NULL,
+    center point DEFAULT point((25)::double precision, ((-25))::double precision) NOT NULL,
+    default_position point DEFAULT point((25)::double precision, ((-25))::double precision) NOT NULL,
+    zoom integer DEFAULT 2 NOT NULL
+);
+
+
+ALTER TABLE public.maps OWNER TO mercator;
+
+--
+-- TOC entry 1891 (class 0 OID 16392)
+-- Dependencies: 162 1892
+-- Data for Name: maps; Type: TABLE DATA; Schema: public; Owner: mercator
+--
+
+COPY maps (id, table_name, name, center, default_position, zoom) FROM stdin;
+\.
+
+
+--
+-- TOC entry 1900 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: maps_seq; Type: SEQUENCE SET; Schema: public; Owner: mercator
+--
+
+SELECT pg_catalog.setval('maps_seq', 1, false);
+
+
+--
+-- TOC entry 1788 (class 2606 OID 16396)
+-- Dependencies: 162 162 1893
+-- Name: maps_pk; Type: CONSTRAINT; Schema: public; Owner: mercator; Tablespace: 
+--
+
+ALTER TABLE ONLY maps
+    ADD CONSTRAINT maps_pk PRIMARY KEY (id);
+
+
+--
+-- TOC entry 1898 (class 0 OID 0)
+-- Dependencies: 5
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+REVOKE ALL ON SCHEMA public FROM postgres;
+GRANT ALL ON SCHEMA public TO postgres;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+-- Completed on 2014-01-09 17:11:22 MSK
+
+--
+-- PostgreSQL database cluster dump complete
+--
+
